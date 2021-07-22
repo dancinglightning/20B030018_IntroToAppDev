aedad1ad103dc12c3d823a17ca8c537b002142c5
Hanan Basheer <20b030018@iitb.ac.in>
2021-07-22 22:42:38 +0530

    Update main.dart

--- Week 2/lib/main.dart
+++ Week 2/lib/main.dart
@@ -1,12 +1,31 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Quiz());
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey,
+        body: Quiz(),
+      ),
+    ),
+  );
 }
 
 class Quiz extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Container());
+    return SafeArea(
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 10.0),
+        child: QuizPage(),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
